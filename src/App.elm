b091d4fa497ee142601890c00aede0bd8e6d2c75
Dennis Snell <dennis.snell@automattic.com>
2016-08-21 14:13:49 -0700

    Use `always` instead of a hockey-stick function

--- src/App.elm
+++ src/App.elm
@@ -98,5 +98,5 @@ main =
         { init = ( initialModel, Cmd.none )
         , view = view
         , update = update
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = always Sub.none
         }
